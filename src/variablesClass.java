4037864012aa4c830450e2fc8b15a8aabcc23025
AlexandruNecula <146215879+AlexandruNecula@users.noreply.github.com>
2023-09-29 15:57:27 +0300

    Initial commit

---
+++ src/variablesClass.java
@@ -0,0 +1,15 @@
+public class variablesClass {
+     long currentWordlPopulation = 7811080815L;
+     int carsProdusedThisYear = 55235358;
+     char firstLetterInAlphabet = 'A';
+     boolean javaIsAmazing = true;
+     float Pi = 3.14159F;
+
+     void printVariables(){
+         System.out.println("Current World Population is: " + currentWordlPopulation);
+         System.out.println("Cars Produced ThisYear: " + carsProdusedThisYear);
+         System.out.println("The First Letter In Alphabet is: " + firstLetterInAlphabet);
+         System.out.println("- Is JAVA amazing? - " + javaIsAmazing);
+         System.out.println("The Pi constant value is: " + Pi);
+     }
+}
